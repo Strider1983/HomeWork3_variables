4ff3c10a4f0cea4e1572cc2b3d94647e741d1202
Konstantin <mezentsev.k@gmail.com>
2023-08-03 11:09:37 +0800

    Задача 1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Типы Переменных");
-
+        System.out.println("Задача№1");
+        int apple = 102364;
+        byte orange = 84;
+        short pine = 17236;
+        long birch = 3152348765L;
+        float weight = 2.298F;
+        double height = 1.45454212454;
+        System.out.println("Значение переменной apple с типом int равно " + apple);
+        System.out.println("Значение переменной orange с типом byte равно " + orange);
+        System.out.println("Значение переменной pine с типом short равно " + pine);
+        System.out.println("Значение переменной birch с типом long равно " + birch);
+        System.out.println("Значение переменной weight с типом float равно " + weight);
+        System.out.println("Значение переменной height с типом double равно " + height);
     }
 }
